a7f56998436b9b171065a59a02d87fff10f3f576
Ben de Vries <mail@bendevries.com>
2020-04-30 16:15:40 +0200

    Added postgres functionality

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from flask import Flask
 from celery import Celery
-from celery.utils.log import get_task_logger
+from flask_sqlalchemy import SQLAlchemy
 import redis
 
-logger = get_task_logger(__name__)
+import psycopg2
+import time
+
+# Add Redis URL configurations
 app = Flask(__name__)
 
-# Add Redis configs
 app.config["CELERY_BROKER_URL"] = "redis://redis:6379/0"
 app.config["CELERY_RESULT_BACKEND"] = "redis://redis:6379/0"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://user:password@postgresdb:5432/db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Wait for postgres container
+while(True):
+    try:
+        conn = psycopg2.connect(app.config["SQLALCHEMY_DATABASE_URI"])
+        print("Postgres connection established")
+        break
+    except:
+        time.sleep(1)
+        print("Waiting for postgres...")
+
+#######################
+# Redis configuration #
+#######################
 # Connect Redis db
 redis_db = redis.Redis(
-    host="redis", port="6379", db=1, charset="utf-8", decode_responses=True
+    host="redis",
+    port="6379",
+    db=1,
+    charset="utf-8",
+    decode_responses=True
 )
 
 # Initialize Celery and update its config
@@ -23,16 +44,58 @@ celery.conf.update(
     timezone="UTC",
     task_serializer="json",
     accept_content=["json"],
-    result_serializer="json",
+    result_serializer="json"
 )
 
+##########################
+# Postgres configuration #
+##########################
+# Postgres
+db = SQLAlchemy(app)
+
+class Node(db.Model):
+    __tablename__ = 'node'
+    id = db.Column(db.Integer, primary_key=True)
+    visits = db.Column(db.Integer)
+
+db.create_all()
+
+#######################
+# Flask configuration #
+#######################
+# Views
 @app.route("/")
 def index_view():
-    return "Flask-celery task scheduler with Postgres database!"
+    return "Flask-celery task scheduler with postgres!"
 
+@app.route("/node/<int:node_id>")
+def node_view(node_id):
+    node = Node.query.get(node_id)
+    if node is None:
+        return "Node is not yet created!"
+    inc_node.delay(node_id)
+    return f"Node {node.id} has been visited: {node.visits} times"
+
+@app.route("/node")
+def new_node_view():
+    create_node.delay()
+    return f"Created new node. Please wait 10 seconds to visit the node :)"
+
+# Tasks
 @celery.task
 def create_node():
-    pass
+    node = Node()
+    node.visits = 0
+    time.sleep(10)
+    db.session.add(node)
+    db.session.commit()
 
+@celery.task
+def inc_node(node_id):
+    node = Node.query.get(node_id)
+    node.visits += 1
+    db.session.commit()
+
+# Run server
 if __name__ == "__main__":
     app.run()
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 ## Flask Celery Postgres Example
 
-This is minimal example of a flask app using Celery for background tasks and a Postgres database that is accessed by flask and celery.
+This is minimal example of a [flask](https://flask.palletsprojects.com/en/1.1.x/) app using [Celery](http://www.celeryproject.org) for background tasks and a [Postgres](https://www.postgresql.org) database that is accessed by flask and celery.
 
 #### Instructions
 To run the application use
 ```sh
 docker-compose up --build
+```
+Access database
+```sh
+dc exec postgresdb psql -U user -d db
 ```
